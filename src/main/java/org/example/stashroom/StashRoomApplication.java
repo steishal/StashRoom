cedbe528611ce167e3bd4fc7ec87bf15615a0dff
steishal <aalinevich@icloud.com>
2025-04-17 17:24:23 +0300

    Init

---
+++ src/main/java/org/example/stashroom/StashRoomApplication.java
@@ -0,0 +1,13 @@
+package org.example.stashroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StashRoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StashRoomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/stashroom/StashRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.stashroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StashRoomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
